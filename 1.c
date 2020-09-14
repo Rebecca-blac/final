ff27f20ad0df60662ff69795fe064e67e2916922
sdfsdf <sjdlkfjsd@slkdfj.com>
2020-09-14 01:17:10 -0700

    added 1.c file

--- 1.c
+++ 1.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdio.h>
 main(){
     double a;
     double b;
@@ -8,21 +7,5 @@ main(){
     scanf("%lf %lf",&a,&b);
     c=(a+b)*(a+b);
     printf("%.2lf\n",c);
-}
 
-int s(int n)
- {
-if(n==0)
-   return 0;
-else
-{
- printf("%d",(n-1)*s(n-1));
-return n;
-}
-}
-void main()
- {
-int a;
-scanf("%d",&a);
-s(a);
- }
+
